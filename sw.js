32a395e04906a62c12a5eddb005b02e83bdc99a2
yessygc <yessgarciacampos@gmail.com>
2022-10-13 21:09:24 -0500

    Navegacion offline con pagina personalizada de error

---
+++ sw.js
@@ -0,0 +1,93 @@
+
+const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+const CACHE_DYNAMIC_LIMIT = 50;
+
+
+function limpiarCache( cacheName, numeroItems ) {
+
+
+    caches.open( cacheName )
+        .then( cache => {
+
+            return cache.keys()
+                .then( keys => {
+                    
+                    if ( keys.length > numeroItems ) {
+                        cache.delete( keys[0] )
+                            .then( limpiarCache(cacheName, numeroItems) );
+                    }
+                });
+
+            
+        });
+}
+
+
+
+
+self.addEventListener('install', e => {
+
+
+    const cacheProm = caches.open( CACHE_STATIC_NAME )
+        .then( cache => {
+
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/img/main.jpg',
+                '/js/app.js',
+                '/img/no-img.jpg',
+                '/pages/offline.html'
+            ]);
+
+        
+        });
+
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
+            .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+
+    e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
+
+});
+
+
+
+self.addEventListener('fetch', e => {
+
+
+    // 2- Cache with Network Fallback
+    const respuesta = caches.match( e.request )
+        .then( res => {
+
+            if ( res ) return res;
+
+            // No existe el archivo
+            // tengo que ir a la web
+            console.log('No existe', e.request.url );
+
+
+            return fetch( e.request ).then( newResp => {
+
+                caches.open( CACHE_DYNAMIC_NAME )
+                    .then( cache => {
+                        cache.put( e.request, newResp );
+                        limpiarCache( CACHE_DYNAMIC_NAME, 50 );
+                    });
+
+                return newResp.clone();
+            });
+
+
+        });
+
+
+
+
+    e.respondWith( respuesta );
+
+})
